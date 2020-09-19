959517cda5d2042477991c289afc64957a1816b7
Ryan Xin <xinpf0715@gmail.com>
2020-09-19 21:33:10 +1000

    first customized page setup

---
+++ src/components/UserPortal.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const UserPortal = (props) => {
+  return(
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
+}; // UserLogin
+
+export default UserPortal;
