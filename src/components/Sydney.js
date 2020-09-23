366c1a9177b3299df061f43745f0a692346383cb
Ryan Xin <xinpf0715@gmail.com>
2020-09-23 17:12:14 +1000

    sydney map show case data

--- src/components/Sydney.js
+++ src/components/Sydney.js
@@ -7,17 +7,47 @@ import axios from 'axios';
 const Sydney = (props) => {
   
   const ALL_CASES_URL = 'http://localhost:1337/cases';
+  const SINGLE_CASE_URL = 'http://localhost:1337/cases';
   
   const [cases, setCases] = useState([]);
   const [currentLocation, setCurrentLocation] = useState({
     lat: -33.86785,
     lng: 151.20732
   })
+  const [currentCase, setCurrentCase] = useState({
+    suburb: '-',
+    location: '-',
+    day: '-',
+    month: '-',
+    year: '-',
+    startTime: '-',
+    endTime: '-',
+  });
   
   const caseIcon = new Icon({
     iconUrl: "/caseIcon.svg",
     iconSize: [25, 25]
   });
+  
+  const handleClick = (e) => {
+    console.log('Clicked');
+    axios.get(`${SINGLE_CASE_URL}/${e.sourceTarget.options.id}`)
+    .then(res => {
+      console.log(res.data.singleCase);
+      const receivedCase = res.data.singleCase;
+      setCurrentCase({
+        ...currentCase,
+        suburb: receivedCase.suburb,
+        location: receivedCase.location,
+        day: receivedCase.day,
+        month: receivedCase.month,
+        year: receivedCase.year,
+        startTime: receivedCase.startTime,
+        endTime: receivedCase.endTime
+      });
+    })
+    .catch(err => console.log(err));
+  }; 
   
   useEffect(() => {
     axios.get(ALL_CASES_URL)
@@ -48,13 +78,21 @@ const Sydney = (props) => {
         />
         {cases.map((c) => (
           <Marker
-            key={c.name}
+            key={c.location}
             position={[c.lat, c.lng]}
             icon={caseIcon}
+            id = {c._id}
+            onClick={handleClick}
           />
         ))}
         <Marker position={[currentLocation.lat, currentLocation.lng]} />
-      </Map>      
+      </Map>
+      <div>
+        <p><strong>Suburb: </strong>{currentCase.suburb}</p>
+        <p><strong>Location: </strong>{currentCase.location}</p>
+        <p><strong>Date: </strong>{currentCase.day} {currentCase.month} {currentCase.year}</p>
+        <p><strong>From </strong>{currentCase.startTime}<strong> to </strong>{currentCase.endTime}</p>
+      </div>
     </div>
   )
 }; // Sydney
--- src/components/World.js
+++ src/components/World.js
@@ -145,12 +145,6 @@ const World = (props) => {
           url="https://api.mapbox.com/styles/v1/ryanxin/ckfdup3bt0d5p19rtln7yawiw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicnlhbnhpbiIsImEiOiJja2ZkdTFhajQwNDh6MnRzaG51ZHFsenByIn0.eqvLzhrjtwg78imgHDi6SQ"
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
         />
-        {/* {cases.map((c) => (
-          <Marker
-            key={c.name}
-            position={[c.countryInfo.lat, c.countryInfo.long]}
-          />
-        ))} */}
         {
           countryCases && countries.map((country) => {
           return <GeoJSON 
